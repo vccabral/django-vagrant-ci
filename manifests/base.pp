de5afc85fe3507ea48f6b16c4a5878c21eb863ed
Andrei Vereha <avereha@gmail.com>
2013-05-01 22:54:38 +0300

    - postgresql installed

--- manifests/base.pp
+++ manifests/base.pp
@@ -9,3 +9,14 @@ include jenkins
 package { "git":
   ensure => installed
 }
+
+class { 'postgresql::server':
+  config_hash => {
+    'ip_mask_deny_postgres_user' => '0.0.0.0/32',
+    'ip_mask_allow_all_users'    => '0.0.0.0/0',
+    'listen_addresses'           => '*',
+    'ipv4acls'                   => ['local all all trust'],
+    'postgres_password'          => 'P@ssword',
+  },
+}
+
