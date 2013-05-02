77ba8051d389ee1e3367d895b0ee5c821dd27a2e
Andrei Vereha <avereha@gmail.com>
2013-05-03 00:24:54 +0300

    - install different python versions

--- manifests/base.pp
+++ manifests/base.pp
@@ -1,7 +1,13 @@
 
+# Configuration variables
+
+$python_version = '2.4' # other options: '2.5', '3.2'.
+
+# End of configuration variables
+
 file { '/etc/motd':
-  content => "Welcome to your Vagrant-built virtual machine!
-  Managed by Puppet.\n"
+  content => "Welcome to django-vagrant-ci virtual machine!
+              Managed by Puppet.\n"
 }
 
 include jenkins
@@ -18,5 +24,20 @@ class { 'postgresql::server':
     'ipv4acls'                   => ['local all all trust'],
     'postgres_password'          => 'P@ssword',
   },
+}
+
+include apt
+# For other versions of Python: 2.4, 2.5, etc
+apt::ppa { "ppa:fkrull/deadsnakes":
+  before     => Class['python']
+}
+
+Class['apt::update'] -> Class['python::install']
+
+class { 'python':
+  version    => $python_version,
+  dev        => false,
+  virtualenv => true,
+  gunicorn   => false
 }
 
