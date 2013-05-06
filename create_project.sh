7b3971a86c1a0edb622fccad39dbd1debec11e37
Andrei Vereha <avereha@gmail.com>
2013-05-06 15:32:02 +0300

    - create the ~/.created_project file. don't install python-* packages

--- create_project.sh
+++ create_project.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-sudo apt-get -y build-dep python-psycopg2
+
+#sudo apt-get -y build-dep python-psycopg2
 . /home/vagrant/virtualenv/icecream/bin/activate
 cd /home/vagrant
 django-admin.py startproject --template=https://github.com/vccabral/django-twoscoops-project/zipball/master --extension=py,rst,html icecream
 cd icecream
 pip install -r requirements/local.txt
+touch ~/.created_project
